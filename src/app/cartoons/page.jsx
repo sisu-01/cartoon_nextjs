2216398aa1010ba15abff1e3fbadf2e907865e5f
sisu-01 <tesv010615@naver.com>
2024-05-13 16:12:54 +0900

    임시 fetch

--- src/app/cartoons/page.jsx
+++ src/app/cartoons/page.jsx
@@ -1,7 +1,23 @@
-const Cartoons = () => {
-    return (
-        <div>Cartoons</div>
-    );
+const getCartoons = async () => {
+  const res = await fetch("http://localhost:4000/api/cartoon");
+
+  if (!res.ok) {
+    throw new Error("err");
+  }
+  return res.json();
+}
+
+const Cartoons = async () => {
+  const cartoons = await getCartoons();
+  return (
+    <div>
+      {cartoons.list.map((cartoon) => (
+        <div>
+          {cartoon.title}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Cartoons;
