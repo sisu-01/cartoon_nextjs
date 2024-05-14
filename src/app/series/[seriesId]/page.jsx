c6cb688e0e6a77377713a35a2b87ab337db2c0a2
sisu-01 <tesv010615@naver.com>
2024-05-14 13:59:39 +0900

    series 임시

--- src/app/series/[seriesId]/page.jsx
+++ src/app/series/[seriesId]/page.jsx
@@ -1,7 +1,34 @@
-const SeriesInfo = () => {
-    return (
-        <div>SeriesInfo</div>
-    );
+export const generateMetadata = async ({params}) => {
+  const {seriesId} = params;
+  const series = await fetch(`http:/localhost:4000/api/list?id=${seriesId}`).then((res) => res.json());
+  return {
+    title: series.list[0].title,
+    description: "슉 슈슉 시",
+  };
+}
+
+const getSeries = async (seriesId) => {
+  const res = await fetch(`http:/localhost:4000/api/list?id=${seriesId}`);
+  if (!res.ok) {
+    throw new Error("getSeries error!");
+  }
+  return res.json();
+}
+
+const SeriesInfo = async ({params}) => {
+  const {seriesId} = params;
+  const series = await getSeries(seriesId);
+  return (
+    <div>
+      {series.list.map((cartoon) => (
+        <div key={cartoon.id}>
+          <a href={`https://gall.dcinside.com/board/view/?id=cartoon&no=${cartoon.id}`}>          
+            {cartoon.title}
+          </a>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default SeriesInfo;
--- src/app/series/page.jsx
+++ src/app/series/page.jsx
@@ -1,7 +1,41 @@
-const Series = () => {
-    return (
-        <div>Series</div>
-    );
+import Link from "next/link";
+
+export const metadata = {
+  title: "Series",
+  description: "시리즈지롱",
+}
+
+const getSeries = async () => {
+  const res = await fetch("http://localhost:4000/api/series");
+  if (!res.ok) {
+      throw new Error("getSeries error!");
+  }
+  return res.json();
+}
+
+  // {
+  //   "id":684074,
+  //   "title":"거북이 수인 만화1",
+  //   "writer_id":"shk3012",
+  //   "writer_nickname":"팬텀",
+  //   "count":2,
+  //   "last_update":"2024-02-27T10:55:57.000Z",
+  //   "average":149
+  // }
+
+const Series = async () => {
+  const seriesJson = await getSeries();
+  return (
+    <div>
+      {seriesJson.list.map((series) => (
+          <div>
+            <Link href={`/series/${series.id}`} key={series.id}>
+              {series.title}
+            </Link>
+          </div>
+      ))}
+    </div>
+  );
 }
 
 export default Series;
