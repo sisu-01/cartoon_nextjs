d5a57ca915e1f669cb74c9dbbae74c65d932dfb5
sisu-01 <tesv010615@naver.com>
2024-06-12 21:58:31 +0900

    페이징 onclick 추가

--- src/components/testPaging/paging.jsx
+++ src/components/testPaging/paging.jsx
@@ -7,8 +7,9 @@ import styles from "./paging.module.css";
  * @param {Number} count 전체 게시글 수
  * @param {Number} pageBtn 페이징 버튼 수
  * @param {String} pathName 경로 ㅋㅋ
+ * @param {Function} onClick 콜백?
  */
-const Paging = ({page, perPage, count, pageBtn, pathName}) => {
+const Paging = ({page, perPage, count, pageBtn, pathName, onClick}) => {
   const pageGroup = Math.ceil(page / pageBtn);//현재 그룹
   const totalPage = Math.ceil(count / perPage);//전체 페이지 개수
   const totalGroup = Math.ceil(totalPage / pageBtn);//전체 그룹 개수
@@ -20,22 +21,36 @@ const Paging = ({page, perPage, count, pageBtn, pathName}) => {
 
   const newArr = [];
   if (pageGroup > 1) {
-    newArr.push(<Link key='first' href={`${pathName}?page=${1}`}>first</Link>);
-    newArr.push(<Link key='prev' href={`${pathName}?page=${startPage-1}`}>prev</Link>);
+    if (onClick) {
+      newArr.push(<div key='first' onClick={() => onClick(1)}>first</div>);
+      newArr.push(<div key='prev' onClick={() => onClick(startPage - 1)}>prev</div>);
+    } else {
+      newArr.push(<Link key='first' href={`${pathName}?page=1`}>first</Link>);
+      newArr.push(<Link key='prev' href={`${pathName}?page=${startPage - 1}`}>prev</Link>);
+    }
   } else {
     newArr.push(<div key='_first'>first</div>);
     newArr.push(<div key='_prev'>prev</div>);
   }
   for (let i = startPage; i <= endPage; i++) {
     if (page === i) {
-        newArr.push(<div key={i}>{i}</div>);
+      newArr.push(<div key={i}>{i}</div>);
     } else {
+      if (onClick) {
+        newArr.push(<div key={i} className={styles.link} onClick={() => onClick(i)}>{i}</div>);
+      } else {
         newArr.push(<Link key={i} href={`${pathName}?page=${i}`}>{i}</Link>);
+      }
     }
   }
   if (pageGroup < totalGroup) {
-    newArr.push(<Link key='next' href={`${pathName}?page=${endPage+1}`}>prev</Link>);
-    newArr.push(<Link key='last' href={`${pathName}?page=${totalPage}`}>last</Link>);
+    if (onClick) {
+      newArr.push(<div key='next' onClick={() => onClick(endPage + 1)}>next</div>);
+      newArr.push(<div key='last' onClick={() => onClick(totalPage)}>last</div>);
+    } else {
+      newArr.push(<Link key='next' href={`${pathName}?page=${endPage + 1}`}>next</Link>);
+      newArr.push(<Link key='last' href={`${pathName}?page=${totalPage}`}>last</Link>);
+    }
   } else {
     newArr.push(<div key='_next'>next</div>);
     newArr.push(<div key='_last'>last</div>);
