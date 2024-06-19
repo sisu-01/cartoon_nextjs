999a9e6339c9ca615dea139dccfb78d6f62be5fc
sisu-01 <tesv010615@naver.com>
2024-06-19 16:20:55 +0900

    파비콘과 김풍

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,16 @@
+import Image from "next/image";
+import styles from "./home.module.css";
+
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className={styles.container}>
+      <Image src="/poong.jpg" alt="" style={{
+        width: '100%',
+        height: 'auto',
+      }}
+      width={400}
+      height={3000} className={styles.img} />
+    </div>
   );
 }
 
