f659dde4a63131e2c4c53465784deced464324c0
sisu-01 <tesv010615@naver.com>
2024-06-21 17:19:59 +0900

    스피너 가운데

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -2,7 +2,7 @@ import Spinner from 'react-bootstrap/Spinner';
 
 const Loading = () => {
   return (
-    <div style={{height: "100vh"}}>
+    <div className="d-flex h-100 align-items-center justify-content-center">
       <Spinner animation="border" role="status">
         <span className="visually-hidden">Loading...</span>
       </Spinner>
