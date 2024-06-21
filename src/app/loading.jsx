625472a060bb1fd71a425938cdd74f41c1c3bef1
sisu-01 <tesv010615@naver.com>
2024-06-21 12:51:37 +0900

    오타수정

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -2,7 +2,7 @@ import Spinner from 'react-bootstrap/Spinner';
 
 const Loading = () => {
   return (
-    <div styles={{height: "100vh"}}>
+    <div style={{height: "100vh"}}>
       <Spinner animation="border" role="status">
         <span className="visually-hidden">Loading...</span>
       </Spinner>
