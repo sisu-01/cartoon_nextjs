dab6d1deaf5dcd65777e3764007452270e2badfa
sisu-01 <tesv010615@naver.com>
2024-06-21 12:44:35 +0900

    스피너

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,6 +1,12 @@
+import Spinner from 'react-bootstrap/Spinner';
+
 const Loading = () => {
   return (
-    <div>Loading</div>
+    <div styles={{height: "100vh"}}>
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    </div>
   );
 }
 
