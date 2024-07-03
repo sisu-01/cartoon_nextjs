014ce6aecb21256af83e18e2a78b1ebf3f317ec4
sisu-01 <tesv010615@naver.com>
2024-07-03 15:10:56 +0900

    카연갤로

--- src/components/share/x/x.jsx
+++ src/components/share/x/x.jsx
@@ -1,6 +1,6 @@
 const X = (param) => {
   const { title, shareUrl, handleClose } = param;
-  const xUrl = `https://x.com/intent/post?text=${encodeURIComponent(title+" (출처 : 카연갤북마크)")}&url=${encodeURIComponent(shareUrl)}`;
+  const xUrl = `https://x.com/intent/post?text=${encodeURIComponent(title+" (출처 : 카툰-연재 갤러리)")}&url=${encodeURIComponent(shareUrl)}`;
   const handle = () => {
     handleClose();
     window.open(xUrl, '_blank', 'width=640,height=440');
