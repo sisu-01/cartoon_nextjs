92924bb25bc2d3f4c9defa9c45ef9bb9f1cf60e2
sisu-01 <tesv010615@naver.com>
2025-03-03 18:46:58 +0900

    페이스북 공유

--- src/components/share/facebook/facebook.jsx
+++ src/components/share/facebook/facebook.jsx
@@ -4,7 +4,7 @@ const app_id = process.env.NEXT_PUBLIC_FACEBOOK;
 
 const Facebook = (param) => {
   const { shareUrl, handleClose } = param;
-  const url =`https://www.facebook.com/share_channel/?link=${encodeURIComponent(shareUrl)}&app_id=${app_id}&source_surface=external_reshare&display=popup&hashtag`;
+  const url =`https://www.facebook.com/share_channel/?type=reshare&link=${encodeURIComponent(shareUrl)}&app_id=${app_id}&source_surface=external_reshare&display&hashtag#`;
  
   const handle = () => {
     handleClose();
