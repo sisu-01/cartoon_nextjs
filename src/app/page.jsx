b9c6b5fe40bd17d537ac36808aeb65435a7ef966
sisu-01 <tesv010615@naver.com>
2026-09-14 19:09:40 +0900

    이미지 최적화

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,12 +4,19 @@ import styles from "./home.module.css";
 const HomePage = () => {
   return (
     <div className={styles.container}>
-      <Image src="/poong.jpg" alt="" style={{
-        width: '100%',
-        height: 'auto',
-      }}
-      width={400}
-      height={3000} className={styles.img} />
+      <Image
+        src="/poong.jpg"
+        alt="카연갤의 기원"
+        width={483}
+        height={3184}
+        sizes="(max-width: 800px) 100vw, 483px"
+        priority
+        style={{
+          width: '100%',
+          height: 'auto',
+        }}
+        className={styles.img}
+        />
     </div>
   );
 }
